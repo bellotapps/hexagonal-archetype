b6b8374650809c5cee2593f53fe0b4bc29f842e6
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-04-11 22:49:04 -0300

    Fix Spring Data Adapter
    - Move MockedUserRepository to upper level in directory structure
    - Add @ComponentScan with package to scan the now moved MockedUserRepository

--- src/main/resources/archetype-resources/__rootArtifactId__-adapters/__rootArtifactId__-spring-data-adapter/src/main/java/spring_data/config/SpringDataConfig.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-adapters/__rootArtifactId__-spring-data-adapter/src/main/java/spring_data/config/SpringDataConfig.java
@@ -4,12 +4,16 @@
 package ${package}.spring_data.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Configuration class for Spring Data Jpa Repositories.
  */
 @Configuration
-// TODO: add @EnableJpaRepositories
-// TODO: add @EntityScan
+@ComponentScan(basePackages = {
+        "${package}.spring_data"
+})
+// TODO: add @EnableJpaRepositories if using Spring Data JPA
+// TODO: add @EntityScan or resources/META-INF/orm.xml file
 public class SpringDataConfig {
 }
